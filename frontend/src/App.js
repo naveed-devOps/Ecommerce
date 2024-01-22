2290732003ad8fa98fb013b5a41c8f24de093da1
Muhammad Naveed <naveed65dev@gmail.com>
2024-01-22 20:09:48 +0500

    update and create file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
- 
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+      <Route path="/product" element={<Product/>}>
+      <Route path=':productId' element={<Product/>}/>
+   </Route>
+    <Route path='/cart' element={<Cart/>}/> 
+    <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
