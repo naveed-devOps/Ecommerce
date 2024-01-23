1cead95953ddc7e2a4cd7dbbab1bad189041a583
Muhammad Naveed <naveed65dev@gmail.com>
2024-01-23 21:03:22 +0500

    update file

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         return (
           <div className="navbar">
             <div className="nav-logo">
-                <img src={logo} alt="" />
+              <img src={logo} alt="" /> 
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Popular from '../Components/Popular/Popular'
+import Offers from "../Components/Offers/Offres";
+import NewCollections from "../Components/NewCollectons/NewCollections";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+ 
 
 const Shop = () => {
     return (
         <div>
             <Hero />
+            <Popular />
+            <Offers />
+            <NewCollections />
+            <NewsLetter />
+             
         </div>
     );
 };
