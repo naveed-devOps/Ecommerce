62d8047a9c1045e03d5c795a64a68d0483fdb420
Muhammad Naveed <naveed65dev@gmail.com>
2024-01-25 15:36:22 +0500

    make screen responsive

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png'; 
 import { Link } from 'react-router-dom';
+import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from '../Assets/drop-down.png';
+
 const Navbar = () => {
                 const [menu,setMenu] = useState("shop");
+                const {getTotalCartItems} = useContext(ShopContext);
+
+                const menuRef = useRef();
+                const dropdown_toggle = (e)=>{
+                  menuRef.current.classList.toggle('nav-menu-visible');
+                  e.target.classList.toggle('open');
+                }
+
         return (
           <div className="navbar">
             <div className="nav-logo">
               <img src={logo} alt="" /> 
                 <p>SHOPPER</p>
             </div>
-            <ul className="nav-menu">
+            <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className="nav-menu">
                 <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration: 'none'}} to="womens">Women</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -20,7 +32,7 @@ const Navbar = () => {
             <div className="nav-login-cart">
                <Link style={{textDecoration: 'none'}} to='/login'><button>Login</button></Link> 
                 <Link style={{textDecoration: 'none'}} to='/cart'><img src={cart_icon} alt="" /></Link>
-                <div className="nav-cart-count">0</div>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
              </div>
           </div>
         );
